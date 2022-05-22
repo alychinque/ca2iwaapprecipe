317dce07475997966a4ebd512b1125747ca323e0
alychinque <alychinque@gmail.com>
2022-05-22 13:06:34 +0000

    Added some packages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faClock)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
