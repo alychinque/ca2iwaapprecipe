d26a71ba3ffa4e437d229078d2afb86e14ed7aae
alychinque <alychinque@gmail.com>
2022-05-22 13:05:54 +0000

    Fixid route in the server

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use('/api/', require('./route/route'))
 
 __dirname = path.resolve()
 if(process.env.NODE_ENV === 'production') {
-    app.get(express.static(__dirname + '/recipe/dist/'))
+    app.get(express.static(__dirname + '/frontend/dist/'))
     app.get('*', (req, res) => {
-        res.sendFile(__dirname + '/recipe/dist/index.html')
+        res.sendFile(__dirname + '/frontend/dist/index.html')
     })
 } else {
     app.get('/', (req, res) => {
